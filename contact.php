054adddaf03bb9644d791c99d86d51f2e8169abf
Jonekkelis <70095766+Jonekkelis@users.noreply.github.com>
2023-02-08 11:49:03 +0200

    Initial commit

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>send email</p>
+    <form class ="contact-form" action="contactform.php" method="post">
+        <input type="text" name="name" placeholder="Full name">
+        <input type="text" name="mail" placeholder="Your Email">
+        <input type="text" name="Subject" placeholder="Subject">
+        <textarea name="message" placeholder="message"></textarea>
+        <button type="submit" name="submit">Send Mail</button>  
+    
+</body>
+</html>
